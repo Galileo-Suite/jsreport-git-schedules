15c7b7f3f1a697424b8526b29dcd981aeb55993a
Michaela DeForest <mdeforest@galileosuite.com>
2022-04-20 09:57:41 -0400

    initial commit

---
+++ jsreport.config.js
@@ -0,0 +1,14 @@
+const defaults = {}
+
+const config = {
+}
+
+module.exports = {
+  name: 'custom',
+  main: './lib/main.js',
+  worker: './lib/worker.js',
+  requires: {
+    core: '3.x.x',
+    studio: '3.x.x'
+  }
+}
